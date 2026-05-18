8c02102225b2c3a50477aafc58ba5b7507bc0277
pasharyan <pasharyan50@gmail.com>
2026-05-18 10:25:25 +0700

    masukkan class lia

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -9,3 +9,14 @@ public:
     //     cout<<"Pesan dari seseorang"<<endl;
     // }
 };
+
+class joko : public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia : public seseorang
