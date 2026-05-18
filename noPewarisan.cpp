7571c26f4c178ea77b9322a5d38a5a61ec31f238
pasharyan <pasharyan50@gmail.com>
2026-05-18 10:56:18 +0700

    masukkan class derivedClass

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -18,3 +18,12 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
+
+class derivedClass : public baseClass1
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
