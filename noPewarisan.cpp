75d8a74912cf1fca899bc35c7443f2c6c6504085
pasharyan <pasharyan50@gmail.com>
2026-05-18 10:55:33 +0700

    masukkan baseClass1

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -9,3 +9,12 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
+
+class baseClass1
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya Function dari base class";
+    }
+};
